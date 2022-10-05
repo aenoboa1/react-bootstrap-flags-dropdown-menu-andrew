bab100f8bfd98899ed27fa2684e4aa0e39621d3d
aenoboa1 <56968012+aenoboa1@users.noreply.github.com>
2022-10-05 13:46:33 -0500

    Lista de Paises

--- index.js
+++ index.js
@@ -9,12 +9,52 @@ import './style.css';
 
 function App() {
   const [countries] = useState([
-    { code: 'gr', title: 'Greece' },
-    { code: 'gb', title: 'United Kingdom' },
-    { code: 'us', title: 'Estados Unidos' },
+    // GROUP A
+    { code: 'qa', title: 'Qatar' },
     { code: 'ec', title: 'Ecuador' },
+    { code: 'sn', title: 'Senegal' },
+    { code: 'nl', title: 'Países Bajos' },
+    // GROUP B
+    { code: 'gb-eng', title: 'Inglaterra' },
+    { code: 'ir', title: 'Iran' },
+    { code: 'us', title: 'Estados Unidos' },
+    { code: 'gb-wls', title: ' Gales' },
+    // GROUP C
+    { code: 'ar', title: 'Argentina' },
+    { code: 'sa', title: 'Arabia Saudita' },
+    { code: 'mx', title: 'México' },
+    { code: 'pl', title: 'Polonia' },
+    // GROUP D
+    { code: 'fr', title: 'Francia' },
+    { code: 'au', title: 'Australia' },
+    { code: 'dk', title: ' Dinamarca' },
+    { code: 'tn', title: 'Tunisia' },
+    // GROUP E
+    { code: 'es', title: 'España' },
+    { code: 'cr', title: 'Costa Rica' },
+    { code: 'de ', title: 'Alemania' },
+    { code: 'jp', title: 'Japon' },
+    // GROUP F
+    { code: 'be', title: 'Belgica' },
+    { code: 'ca', title: 'Canada' },
+    { code: 'ma', title: 'Marruecos' },
+    { code: 'hr', title: 'Croacia' },
+    // Group G
+    { code: 'br', title: 'Brasil' },
+    { code: 'rs', title: 'Serbia' },
+    { code: 'ch', title: 'Suiza' },
+    { code: 'cm', title: 'Cameroon' },
+    // Group H
+    { code: 'pt', title: 'Portugal' },
+    { code: 'gh', title: 'Ghana' },
+    { code: 'uy', title: 'Uruguay' },
+    { code: 'kr', title: 'Corea del Sur' },
   ]);
-  const [toggleContents, setToggleContents] = useState('Select a country');
+
+  const [toggleContents, setToggleContents] = useState(
+    'Seleccione el Pais Ganador'
+  );
+
   const [selectedCountry, setSelectedCountry] = useState();
 
   return (
